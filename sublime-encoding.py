51e7f22d521c8fb4b173ffeebba6d6f96a637cc9
Rob Frawley 2nd <rfrawley@scribenet.com>
2013-09-18 13:15:36 -0400

    slight changes to push v0.1.0

--- sublime-encoding.py
+++ sublime-encoding.py
@@ -3,5 +3,5 @@ import sublime_plugin
 
 class ShowFileEncodingCommand(sublime_plugin.WindowCommand):
     def run(self):
-    	sublime.message_dialog('Current file encoding: '+self.window.active_view().encoding())
+    	sublime.message_dialog('Current file and encoding: '+self.window.active_view().encoding())
         return
